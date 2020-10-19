89df1b1ed0e38fae26f814a5923288a737b71184
jtirkkon <jouni.tirkkonen@gmail.com>
2020-10-19 12:31:19 +0300

    baseurl change

--- src/services/persons.js
+++ src/services/persons.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-const baseUrl =  'https://evening-savannah-71492.herokuapp.com/api/persons'
+//const baseUrl =  'https://evening-savannah-71492.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 //Ennen herokua local: const baseUrl = 'http://localhost:3001/api/persons'
 //Heroku: https://evening-savannah-71492.herokuapp.com/
